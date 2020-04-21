fd890fcb83bbde6040f73f592c9b032bd5ea1826
unknown <el.salaheddine@hotmail.com>
2020-04-21 04:02:57 +0200

    création de la page rap groups et des pages person pour les groupes de rap

--- js/rap/rapGroups.js
+++ js/rap/rapGroups.js
@@ -1,37 +1,79 @@
-var imgContainer = document.getElementById("info-groups");
-var arrimg = imgContainer.getElementsByTagName("img");
-var arrDescription = imgContainer.getElementsByTagName("p");
-setImgSize()
-setParagraph()
-var xhr = new XMLHttpRequest();
-function getJson(){
-    xhr.onreadystatechange = function()
-    {
-        if(this.readyState == 4 && this.status == 200)
-        {
-            var rapGroups = JSON.parse(this.responseText);
-            var rgroups = rapGroups.rapGroups;
-            for(i =0;i<rgroups.length;i++)
-            {
-                arrimg[i].setAttribute("src",rgroups[i].image);
-                arrDescription[i].innerHTML = rgroups[i].miniDescription;
-            }
-        }
+/* La méthode getRapGroups() va recuper les info des groupes de rap et va les manipuler avec du dom afin de les afficher.*/
+
+
+function getRapGroups(xhttp) {
+    var imgContainer = document.getElementById("info-groups");
+    var arrimg = imgContainer.getElementsByTagName("img");
+    var arrDescription = imgContainer.getElementsByTagName("p");
+    var arrLink = imgContainer.getElementsByTagName("a");
+    var rgroups = xhttp;
+    for (i = 0; i < rgroups.rapGroups.length; i++) {
+        arrimg[i].setAttribute("src", rgroups.rapGroups[i].image);
+        arrDescription[i].innerHTML = rgroups.rapGroups[i].miniDescription;
+        arrimg[i].style.width = "284px";
+        arrimg[i].style.height = "177px";
+        arrLink[i].setAttribute("href", rgroups.rapGroups[i].lien);
     }
-    xhr.open("GET","../../json/groups.json",true);
-    xhr.send();
 }
 
-function setImgSize(){
-    for(i=0;i<arrimg.length;i++)
-    {
-        arrimg[i].style.width = "284px"
-        arrimg[i].style.height= "177px"
+
+
+
+/* La méthode getIam() va recuperer dans le serveur les information concernant le groupe Iam et va les afficher avec du dom dans le neviguateur*/
+function getIam(response) {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    var i;
+    switch (page) {
+        case "iam.html":
+            i = 0;
+            break;
+        case "psy.html":
+            i = 1;
+            break;
+        case "arsenik.html":
+            i = 2;
+            break;
+        case "fonky.html":
+            i = 3;
+            break;
+        case "assaut.html":
+            i = 4;
+            break;
+        default:
+            i = 0;
+
     }
+    var iamImage = document.getElementById("Img");
+    var iamDescription = document.getElementById("iamDescription");
+    iamDescription.innerHTML = response.rapGroups[i].description;
+    iamImage.setAttribute("src", response.rapGroups[i].imagePerso);
+    ajaxRequest("../../json/discographie.json", getdisco, response.rapGroups[i].name);
 }
-function setParagraph(){
-    for(i=0;i<arrDescription.length;i++)
-    {
-        arrDescription[i].style.textAlign = "center";
+
+function getdisco(response, nom) {
+
+
+    var discographie = document.getElementById("disco");
+
+    for (i = 0; i < response[nom].length; i++) {
+
+        discographie.innerHTML += "<span class='span1'>" + response[nom][i].date + "</span>" + ": " + "<span class='span2'>" + response[nom][i].titre + "</span>" + "<br>"
     }
+
+
+
+}
+
+/*La méthode ajaxRequest()  fait une requête ajax  pour recuperer un fichier json passer en paramètre. Elle retourne la réponse de la requête*/
+function ajaxRequest(url, fonction, args = "0") {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            fonction(JSON.parse(this.responseText), args);
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
 }
